d853100f8e76b724e1aed344237e82205fde15aa
Thijs ter Horst <gmterhorst@gmail.com>
2016-05-02 12:12:20 +0200

    update

--- Assignment1/sim1.m
+++ Assignment1/sim1.m
@@ -23,12 +23,16 @@ colors = ['y' 'm' 'c' 'r' 'k' 'b'];
 figure
 
 for k = 1:length(samples)
-
+    
+    % Generate an x array for the training and x_ext (x_extended) to see
+    % how the estimate will perform outside the training boundaries
     x = linspace(0.01, 0.99, samples(k))';
+    x_ext = linspace(0.01, 1.99, 2*samples(k))';
 
     % Choose a function ln(1/x) (log in matlab returns ln)
     % Choose because of taylor series -> higher polynomial should do better
     y_act = log(x.^-1);
+    y_ext = log(x_ext.^-1);
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Generate n datasets y_samp, sigma = 1 %
@@ -46,9 +50,17 @@ for k = 1:length(samples)
     A{4} = [ ones(samples(k), 1), x, x.^2 x.^3 x.^4 ];
     A{5} = [ ones(samples(k), 1), x, x.^2 x.^3 x.^4 x.^5 ];
     A{6} = [ ones(samples(k), 1), x, x.^2 x.^3 x.^4 x.^5 x.^6];
+
+    B{1} = [ ones(2*samples(k), 1), x_ext ];
+    B{2} = [ ones(2*samples(k), 1), x_ext, x_ext.^2 ];
+    B{3} = [ ones(2*samples(k), 1), x_ext, x_ext.^2 x_ext.^3 ];
+    B{4} = [ ones(2*samples(k), 1), x_ext, x_ext.^2 x_ext.^3 x_ext.^4 ];
+    B{5} = [ ones(2*samples(k), 1), x_ext, x_ext.^2 x_ext.^3 x_ext.^4 x_ext.^5 ];
+    B{6} = [ ones(2*samples(k), 1), x_ext, x_ext.^2 x_ext.^3 x_ext.^4 x_ext.^5 x_ext.^6];
+
     
     subplot(2,2,k);
-    xlim([0 1]);
+    xlim([0 1.2]);
     hold on;
     grid on;
 
@@ -57,8 +69,9 @@ for k = 1:length(samples)
         
         % matlab returns a least squares solution here, 
         % since the matrix dimensions will not match
-        beta = A{i}\y_samp;
+        beta = A{i} \ y_samp;
         y_hat = A{i} * beta;
+        y_est = B{i} * beta;
 
         % bias and variance taken from page 50 of the book. Because we have
         % n datasets instead of one, I added an extra 'mean' around the 
@@ -72,8 +85,9 @@ for k = 1:length(samples)
         mse(i, k) = bias_squared(i, k) + variance(i, k);
         mse2(i,k) = mean(mean((repmat(y_act, 1, n) - y_hat).^2));
 
-        % plot y_hat
-        plot(x, y_hat(:,1), colors(i));
+        % plot y_hat and y_est
+        %plot(x, y_hat(:,1), colors(i));
+        plot(x_ext, y_est(:,1), colors(i));
         title([num2str(samples(k)) ' samples']);
         xlabel('x');
         ylabel('y');
