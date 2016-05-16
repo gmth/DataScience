bfb630d451bbbe1982032f56324a41be0317643d
Thijs ter Horst <gmterhorst@gmail.com>
2016-05-16 10:52:02 +0200

    added coeffs value that holds the coefficients for the error fit. they're pretty constant ~a=1.4, b = -0.28

--- Assignment2/sim1.m
+++ Assignment2/sim1.m
@@ -9,20 +9,30 @@ a = 3;
 x_test = 0;
 nSamples = 1000;
 
-for i = 1:length(p)
-    
-    x_samples     = unifrnd(0, 1, [nSamples, p(i)]);
-    x_samples_mag = sqrt(sum(x_samples.*x_samples, 2)); 
-    y = exp(-a * x_samples_mag);
+numtests = 20;
+coeffs = zeros(numtests, 2);
 
-    x_closest = min(abs(x_samples_mag - x_test));
-    predictions(i) = exp(-a * x_closest);
+for j = 1:numtests
 
+
+    for i = 1:length(p)
+        
+        x_samples     = unifrnd(0, 1, [nSamples, p(i)]);
+        x_samples_mag = sqrt(sum(x_samples.*x_samples, 2)); 
+        y = exp(-a * x_samples_mag);
+
+        x_closest = min(abs(x_samples_mag - x_test));
+        predictions(i) = exp(-a * x_closest);
+
+    end
+
+%    plot(p, predictions);
+
+    f = fit(p', predictions', 'exp1');
+    coeffs(j,:) = coeffvalues(f);
+
+%    plot(f, p, predictions);
 end
 
-figure
-plot(p, predictions);
+coeffs
 
-f = fit(p', predictions', 'exp1');
-
-plot(f, p, predictions);
