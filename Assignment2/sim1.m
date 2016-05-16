5c30b824df583b12ed229e857c3acf3455d96c51
Thijs ter Horst <gmterhorst@gmail.com>
2016-05-16 10:22:20 +0200

    first figure + script for points 1-4 of assignment

---
+++ Assignment2/sim1.m
@@ -0,0 +1,25 @@
+
+p = [ 1 2 4 8 16 32 ];
+predictions = zeros(1,length(p));
+
+a = 3;
+x_test = 0;
+nSamples = 1000;
+
+for i = 1:length(p)
+
+    
+    x_samples     = unifrnd(0, 1, [nSamples, p(i)]);
+    x_samples_mag = sqrt(sum(x_samples.*x_samples, 2)); 
+    y = exp(-a * x_samples_mag);
+
+    x_closest = min(abs(x_samples_mag - x_test));
+    predictions(i) = exp(-a * x_closest);
+
+end
+
+figure
+plot(p, predictions);
+
+
+
